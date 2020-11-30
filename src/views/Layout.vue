81a8246973aeca9b2605ee109ae7ba6fdd2e7c8e
pratamanidhi <bimatitani88@gmail.com>
2020-11-30 12:30:27 +0700

    bikin table boostrap vue

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-        <b-row>
-            <b-col md="2">
-                <Sidebar />
-            </b-col>
-            <b-col md="10">
-                <slot>
-                </slot>
-            </b-col>
-        </b-row>
-    </div>
+  <div class="backgroundImage">
+    <b-row>
+      <b-col md="2">
+        <Sidebar />
+      </b-col>
+      <b-col md="10">
+        <slot> </slot>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
-    import Sidebar from "@/views/auth/Sidebar.vue"
-    export default {
-        name: 'layout',
-        components: {
-            Sidebar,
-        }
-    }
+import Sidebar from "@/views/auth/Sidebar.vue";
+export default {
+  name: "layout",
+  components: {
+    Sidebar,
+  },
+};
 </script>
+<style >
+.backgroundImage {
+  background-image: url("E:/dashboard/src/assets/ewa1.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
